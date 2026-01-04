d7744b8a37dfeff1193fc47ce6c24a364f323a52
Vagner Souza <juninfps0@gmail.com>
2026-01-04 13:39:03 -0300

    Classe ServiceLivro com metodos funcionando OK

--- src/service/ServiceLivro.java
+++ src/service/ServiceLivro.java
@@ -3,18 +3,57 @@ package service;
 import model.Livro;
 import repository.LivroRepository;
 
+import java.util.List;
+
 public class ServiceLivro {
 
-    private LivroRepository livroRepository = new LivroRepository<>();
+    private LivroRepository repository;
 
-    public void registrarLivro(Livro livro){
-        livroRepository.salvarLivro(livro);
+    public ServiceLivro(LivroRepository repository) {
+        this.repository = repository;
     }
 
-    public void listarLivrosDisponiveis(){
-
+    public void cadastrarLivro(String titulo) {
+        Livro livro = new Livro(titulo);
+        repository.salvar(livro);
     }
 
+    public String emprestarLivro(String titulo) {
+        Livro livro = repository.buscarPorTitulo(titulo);
 
+        if (livro == null) {
+            return "Livro não encontrado";
+        }
 
+        if (livro.isEmprestado()) {
+            return "Livro já está emprestado";
+        }
+
+        livro.emprestar();
+        return "Livro emprestado com sucesso";
+    }
+
+    public String devolverLivro(String titulo) {
+        Livro livro = repository.buscarPorTitulo(titulo);
+
+        if (livro == null) {
+            return "Livro não encontrado";
+        }
+
+        livro.devolver();
+        return "Livro devolvido com sucesso";
+    }
+
+    public List<Livro> listarTodos(){
+        return repository.listar();
+    }
 }
+
+
+
+
+
+
+
+
+
